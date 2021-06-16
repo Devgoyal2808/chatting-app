203a9514c9a18b8769305bbdbd52e67949943870
Devgoyal <Devgoyal2808@github.com>
2021-06-16 17:56:19 +0530

    head foot layout coomit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app=express();
 const port=8000;
+const expressLayouts=require('express-ejs-layouts');
 
+app.use(expressLayouts);
 // use express router
 app.use('/',require('./routes'));
+
+
+//set up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
